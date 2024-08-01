1b21ddd30ba819058054814cce6cae1f9b017036
Dilara <dilara.cankaya@gmail.com>
2024-08-01 17:06:57 +0300

    second week run

--- common.py
+++ common.py
@@ -1,37 +1,17 @@
 from warnings import filterwarnings
-import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
 import tweepy
 from nltk.corpus import stopwords
 from nltk.sentiment import SentimentIntensityAnalyzer
 from textblob import Word, TextBlob
-from wordcloud import WordCloud
-from dotenv import load_dotenv
-import os
 filterwarnings('ignore')
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
-load_dotenv()  # Load environment variables from .env file
 
-api_key = os.getenv('api_key')
-api_key_secret = os.getenv('api_key_secret')
-access_token = os.getenv('access_token')
-access_token_secret = os.getenv('access_token_secret')
-bearer_token = os.getenv('bearer_token')
-
-
-
-# Set up Tweepy client for API v2
-client = tweepy.Client(bearer_token=bearer_token)
-
-
-
-# FUNCTIONS
 def fetch_tweets(query, count=100):
     tweet_list = []
     next_token = None
@@ -57,8 +37,7 @@ def fetch_tweets(query, count=100):
                 retweets = tweet.public_metrics.get('retweet_count', None)
                 likes = tweet.public_metrics.get('like_count', None)
                 user_info = user_dict.get(tweet.author_id)
-                followers = user_info.public_metrics.get('followers_count', None) if user_info and hasattr(user_info,
-                                                                                                           'public_metrics') else None
+                followers = user_info.public_metrics.get('followers_count', None) if user_info and hasattr(user_info, 'public_metrics') else None
 
                 tweet_info = {
                     "Date": tweet.created_at,
@@ -66,14 +45,12 @@ def fetch_tweets(query, count=100):
                     "Tweet": tweet.text,
                     "Retweets": retweets,
                     "Likes": likes,
-                    "Followers": followers
-                }
+                    "Followers": followers}
                 tweet_list.append(tweet_info)
 
             next_token = response.meta.get('next_token', None)
             if not next_token:
                 break
-
             remaining_tweets -= max_results
 
         except tweepy.TooManyRequests:
@@ -89,9 +66,11 @@ def fetch_tweets(query, count=100):
     df = pd.DataFrame(tweet_list, columns=["Date", "User", "Tweet", "Retweets", "Likes", "Followers"])
     return df
 
+
 def analyze_sentiment(tweet):
     analysis = TextBlob(tweet)
     return analysis.sentiment.polarity
+
 
 def classify_tweet(tweet, safety_keywords, capability_keywords):
     has_capability = any(keyword in tweet for keyword in capability_keywords)
@@ -106,12 +85,15 @@ def classify_tweet(tweet, safety_keywords, capability_keywords):
     else:
         return "Neither"
 
-def process_df(analysis):
-    if analysis == "psa":
-        dataframe = tweets_df_psa
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def process_df(analysis_type, dataframe):
+    if analysis_type == "psa":
         safety_keywords = general_safety_alignment_terms + technical_safety_terms
-    if analysis == "rsa":
-        dataframe = tweets_df_rsa
+    elif analysis_type == "rsa":
         safety_keywords = general_safety_alignment_terms + regulatory_policy_terms
 
     # Normalizing Case Folding
@@ -128,8 +110,7 @@ def process_df(analysis):
     sw = stopwords.words('english')
     dataframe['Tweet'] = dataframe['Tweet'].apply(lambda x: " ".join(x for x in str(x).split() if x not in sw))
 
-    # Rarewords
-    # TODO cancelled Rareword processing because it's a small dataset and I don't want to lose words
+    # Rarewords -- cancelled Rareword processing because it's a small dataset and I don't want to lose words
     # temp_df = pd.Series(' '.join(dataframe['Tweet']).split()).value_counts()
     # drops = temp_df[temp_df <= 1]
     # dataframe['Tweet'] = dataframe['Tweet'].apply(lambda x: " ".join(x for x in x.split() if x not in drops))
@@ -150,11 +131,11 @@ def process_df(analysis):
             "polarity_score"] if row["polarity_score"] < 0 and row["Classification"] == "Capabilities" else row[
             "polarity_score"], axis=1)
 
-    # Normalize Stance_-1to1 to between 0 to 100
-    dataframe["Stance_0to100"] = ((dataframe["Stance_-1to1"] + 1) / 2 * 100).round(2)
-    dataframe["Stance_weighted_reach"] = dataframe["Stance_0to100"] * np.log(dataframe['Followers'] + 1)
-    # TODO add line to accommodate retweet count bc I will pull once more without RTs but with just the RT numbers
+    influence_factor = sigmoid(np.log(dataframe['Followers'] + 1) + np.log(dataframe['Retweets'] + 1))
+    dataframe["Stance_weighted_reach_-1to1"] = (dataframe["Stance_-1to1"] * influence_factor)
+    dataframe["Stance_weighted_0to100"] = ((dataframe["Stance_weighted_reach_-1to1"] + 1) / 2 * 100).round(2)
     return dataframe
+
 
 def recalculate_reclassification(dataframe):
     dataframe["Stance_-1to1"] = dataframe.apply(
@@ -162,21 +143,18 @@ def recalculate_reclassification(dataframe):
             "polarity_score"] if row["polarity_score"] < 0 and row["Classification"] == "Capabilities" else row[
             "polarity_score"], axis=1)
 
-    # Normalize Stance_-1to1 to between 0 to 100
-    dataframe["Stance_0to100"] = ((dataframe["Stance_-1to1"] + 1) / 2 * 100).round(2)
-    dataframe["Stance_weighted_reach"] = dataframe["Stance_0to100"] * np.log(dataframe['Followers'] + 1)
-
+    influence_factor = sigmoid(np.log(dataframe['Followers'] + 1) + np.log(dataframe['Retweets'] + 1))
+    dataframe["Stance_weighted_reach_0to1"] = (dataframe["Stance_-1to1"] * influence_factor).round(2)
+    dataframe["Stance_0to100"] = (dataframe["Stance_weighted_reach_0to1"] * 100).round(2)
     return dataframe
+
 
 def change_classification(dataframe, search_string, new_classification):
     dataframe.loc[dataframe["Tweet"].str.contains(search_string, case=False), "Classification"] = new_classification
 
+
 def save_to_csv(dataframe, filename):
     dataframe.to_csv(filename, index=False)
-
-
-
-date_dmy = datetime.today().strftime('%d%m%y')
 
 
 # Dictionaries of keywords related to AI capabilities and AI safety
@@ -210,133 +188,3 @@ technical_safety_terms = [
 regulatory_policy_terms = [
     'AI policy', 'legislative framework', 'compliance standard', 'AI ethic board', 'regulatory compliance', 'risk assessment', 'safety standard', 'policy recommendation',
     'AI oversight', 'international cooperation', 'AI treaty']
-
-# first keyword lists:
-# capability_keywords = ['innovation', 'innovative', 'progress', 'progressive', 'technology', 'technological', 'capability', 'capable', 'efficiency', 'efficient', 'productivity', 'productive']
-# safety_keywords = ['safe', 'safety', 'risk', 'ethic', 'ethical', 'regulate', 'regulation', 'secure', 'security', 'harm', 'control']
-
-"""
-def score_tweet(tweet, capability_keywords, safety_keywords):
-    capability_score = sum(1 for word in capability_keywords if word in tweet.lower())
-    safety_score = sum(1 for word in safety_keywords if word in tweet.lower())
-    return capability_score, safety_score
-
-def classify_sentiment(polarity): # TODO do I really need this? -- can I not use sia?
-    if polarity > 0:
-        return 'Positive'
-    elif polarity < 0:
-        return 'Negative'
-    else:
-        return 'Neutral'
-
-def classify_stance(capability_score, safety_score):
-    if capability_score > safety_score:
-        return 'AI Capabilities'
-    elif safety_score > capability_score:
-        return 'AI Safety'
-    else:
-        return 'Neutral'
-"""
-"""
-tweets_df["Tweet"].iloc[5392]
-df["polarity_score"].iloc[3078]
-df["Stance_0to100"].iloc[3078]
-
-condition = (((df["Classification"] == "Both") | (df["Classification"] == "Neither")) & ((df["Stance_0to100"] < 35) | (df["Stance_0to100"] > 65)))
-indexes = df[condition].index
-df_loc = tweets_df.loc[indexes, ["Tweet", "Retweets"]]
-df_loc.shape
-
-condition1 = ((df["Classification"] == "Neither") & (df["Stance_0to100"] < 35))
-indexes1 = df[condition1].index
-df_loc1 = tweets_df.loc[indexes1, ["Tweet", "Retweets"]]
-
-df["Stance_-1to1"].iloc[2825]
-df["Stance_0to100"].iloc[2825]
-df["Classification"].iloc[3078]
-df["Tweet"].iloc[3078]
-"""
-"""
-#### buraya kadar çalıştırdım, error veriyor çünkü access yok.
-
-# Prepare CSV file
-with open('tweets.csv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Tweet", "Sentiment"])
-
-    # Process each tweet
-    for tweet in tweets.data:
-        # Perform sentiment analysis
-        analysis = TextBlob(tweet.text)
-        sentiment = analysis.sentiment.polarity
-
-        # Write to CSV
-        writer.writerow([tweet.text, sentiment])
-
-print("Tweets have been saved to tweets.csv")
-
-### kısa blok bitiş
-"""
-"""
-# Plot stance over time
-tweets_df['Date'] = pd.to_datetime(tweets_df['Date'])
-tweets_df.set_index('Date', inplace=True)
-tweets_df.resample('D').size().unstack().plot(kind='bar', stacked=True)
-plt.title('Stance Over Time')
-plt.xlabel('Date')
-plt.ylabel('Number of Tweets')
-plt.show()
-
-# Plot sentiment over time
-tweets_df['Date'] = pd.to_datetime(tweets_df['Date'])
-tweets_df.set_index('Date', inplace=True)
-tweets_df.resample('D').mean()['Sentiment'].plot()
-plt.title('Sentiment Over Time')
-plt.xlabel('Date')
-plt.ylabel('Average Sentiment')
-plt.show()
-"""
-"""
-############### burası da ilk kodum
-
-# Define your queries
-queries = [
-    "AI policy",
-    "AI ethics",
-    "AI regulation",
-    "AI safety",
-    "AI governance framework",
-    "#AIgovernance",
-    "#AIethics",
-    "#AIpolicy"
-    "AI law"
-]
-
-# Fetch tweets for each query and save to CSV
-for query in queries:
-    tweets_df = fetch_tweets(query, count=100)
-    if not tweets_df.empty:
-        # Clean the query string for filename
-        query_filename = query.replace(" ", "_").replace("#", "")
-        save_to_csv(tweets_df, f"tweets_{query_filename}.csv")
-    else:
-        print(f"No tweets found for query: {query}")
-"""
-"""
-# Fetch tweets
-safe_query = "AI governance" # TODO need to change this
-safe_tweets_df = fetch_tweets(safe_query, count=100)
-safe_tweets_df.to_csv("safe_tweets.csv", index=False)
-
-cap_query = "AI capabilities" # TODO need to change this
-cap_tweets_df = fetch_tweets(cap_query, count=100)
-safe_tweets_df.to_csv("cap_tweets.csv", index=False)
-
-# Analyze sentiment
-tweets_df['Sentiment'] = tweets_df['Tweet'].apply(analyze_sentiment)
-tweets_df['Sentiment_Class'] = tweets_df['Sentiment'].apply(classify_sentiment)
-
-# Score tweets
-tweets_df[['Capability_Score', 'Safety_Score']] = tweets_df['Tweet'].apply(lambda tweet: pd.Series(score_tweet(tweet, capability_keywords, safety_keywords)))
-tweets_df['Stance'] = tweets_df.apply(lambda row: classify_stance(row['Capability_Score'], row['Safety_Score']), axis=1)
-"""
--- indicators.py
+++ indicators.py
@@ -1,230 +1,105 @@
 from common import *
+import os
+import calendar
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 # ! "If a search request does not specify a start_time, end_time, or since_id request parameter, the end_time will default to "now" (actually 30 seconds before the time of query) and the start_time will default to seven days ago."
 
+load_dotenv("credentials.env")
 
-## REGULATORY SENTIMENT ANALYSIS
+api_key = os.getenv('api_key')
+api_key_secret = os.getenv('api_key_secret')
+access_token = os.getenv('access_token')
+access_token_secret = os.getenv('access_token_secret')
+bearer_token = os.getenv('bearer_token')
+
+client = tweepy.Client(
+    bearer_token=bearer_token,
+    consumer_key=api_key,
+    consumer_secret=api_key_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret)
+
+
+# Calculate the number of tweets to pull weekly
+monthly_tweet_topull = 10000
+
+# How many tweets should be pulled now? Calculate the number of Mondays in the current month
+year = datetime.today().year
+month = datetime.today().month
+num_mondays = sum(1 for day in calendar.monthcalendar(year, month) if day[calendar.MONDAY] != 0)
+
+weekly_tweet_topull = monthly_tweet_topull / num_mondays
+weekly_psa_topull = weekly_rsa_topull = weekly_tweet_topull/2
+
+date_dmy = datetime.today().strftime('%d%m%y')
+
+# REGULATORY SENTIMENT ANALYSIS
 query_rsa = "AI (regulation OR regulations OR regulatory OR policy OR policies OR framework OR frameworks OR government OR governance OR legislation OR laws OR compliance OR oversight OR standards OR ethics OR ethical OR guidelines OR safety OR audit OR accountability OR transparency OR risk OR management OR assessment OR cooperation OR treaty OR treaties) -is:retweet lang:en -decentralized -DAO -crypto -cryptocurrency -cryptocurrencies -#crypto -#cryptocurrency -L2 -#L2 -#Layer2"
+file_path = f"fetched_tweets/tweets_rsa_{date_dmy}.csv"
 
-# Fetch tweets
-# tweets_df_rsa = fetch_tweets(query_rsa, count=4209)
-# save_to_csv(tweets_df_rsa,f"fetched_tweets/tweets_rsa_{date_dmy}.csv")
-# df_rsa = tweets_df_rsa.copy()
+if not os.path.exists(file_path):
+    tweets_df_rsa = fetch_tweets(query_rsa, count=weekly_rsa_topull)
+    save_to_csv(tweets_df_rsa, file_path)
+else:
+    tweets_df_rsa = pd.read_csv(file_path)
+    tweets_df_rsa.head()
 
-# Read saved tweets dataset
-#tweets_df_rsa = pd.read_csv("fetched_tweets/tweets_rsa_{??}.csv")
-#tweets_df_rsa.head()
-
-# Process
-# process_df("rsa").head()
-# TODO tweets_df_rsa["Stance_weighted_reach"].describe().T
+process_df("rsa", tweets_df_rsa).head()
+tweets_df_rsa["Stance_weighted_0to100"] = tweets_df_rsa["Stance_weighted_0to100"].round(2)
+tweets_df_rsa["Stance_weighted_0to100"].describe().T
 
 # Manual tweaks specific to this dataset
 """
 #df.loc[df["Tweet"].str.contains("firstever global ai summit last year")]
 change_classification(tweets_df_rsa, "exploited consumer bluwhale leverage", "Safety")
-change_classification(tweets_df_rsa, "partnering push boundary web3 ai ten offer l2", "Safety")
-change_classification(tweets_df_rsa, "taylor swift lead heavy regulation ai", "Safety")
-change_classification(tweets_df_rsa, "completely unacceptable rely ai translation manga industry fav", "Safety")
-change_classification(tweets_df_rsa, "mam file fir handle sharing deepfake", "Safety")
-change_classification(tweets_df_rsa, "surgical strike fake video coming soon minister", "Safety")
-# TODO WHY WASNT THIS ABOVE LABELLED SAFETY? IT'S SO OBVIOUS
-change_classification(tweets_df_rsa, "get twisted ai fascism fully buddied reason america", "Capabilities")
-change_classification(tweets_df_rsa, "firstever global ai summit last year laid vision future", "Safety")
-change_classification(tweets_df_rsa, "natural state free market deflation everything stop political ", "Capabilities")
-change_classification(tweets_df_rsa, "hunger rt like pushed towards circulating ai generated", "Safety")
-change_classification(tweets_df_rsa, "collaboration centralized exchange grok reveals copxdao", "Capabilities")
-change_classification(tweets_df_rsa, "nearprotocol latest quill ilblackdragon cofounder", "Capabilities")
-change_classification(tweets_df_rsa, "ai legislation tracker new ai law introduced worldwide", "Safety")
-change_classification(tweets_df_rsa, "good morning amitshah ji goi use human ai generate", "Capabilities")
-change_classification(tweets_df_rsa, "inconvenient truth scraping essential ai violates nearly core privacy", "Capabilities")
-change_classification(tweets_df_rsa, "regulating ai paper wchristmarsden ai coregulation", "Safety")
-change_classification(tweets_df_rsa, "china watching return policy old communist system known time", "Capabilities")
-change_classification(tweets_df_rsa, "chat control 2 sommaruppdatering", "Safety")
-change_classification(tweets_df_rsa, "im testifying u senate thursday ai need privacy law", "Safety")
-change_classification(tweets_df_rsa, "marketplace improving efficiency transparency automates", "Safety")
-
 recalculate_reclassification(tweets_df_rsa)
 """
-
 # tweets_df_rsa.groupby("Classification")["Stance_0to100"].agg(["count", "mean"])
 
 # Final score of RSA
 hist_rsa = [40.12, 42.15, 43.67, 44.88, 46.20, 47.35, 48.58, 49.12, 49.87, 50.24, 50.67, 51.22, 50.89]
-# final_rsa = tweets_df_rsa["Stance_0to100"].mean() # 50.89
-final_rsa = 50.89
+final_rsa = tweets_df_rsa["Stance_weighted_0to100"].mean().round(2)
+hist_rsa.append(final_rsa)
 
 
 ## PUBLIC SENTIMENT ANALYSIS
 query_psa = "AI (governance OR policy OR regulation OR capabilities) OR (#AIgovernance OR #AIpolicy) -is:retweet lang:en -decentralized -DAO -blockchain -#blockchain -blockchainassn -crypto -cryptocurrency -cryptocurrencies -#crypto -#cryptocurrency -L2 -#L2 -#Layer2"
+file_path = f"fetched_tweets/tweets_psa_{date_dmy}.csv"
 
-# Fetch tweets
-# tweets_df_psa = fetch_tweets(query_psa, count=500)
-# save_to_csv(tweets_df_psa,f"fetched_tweets/tweets_psa_{date_dmy}.csv")
-# df_psa = tweets_df_psa.copy()
+if not os.path.exists(file_path):
+    tweets_df_psa = fetch_tweets(query_psa, count=weekly_psa_topull)
+    save_to_csv(tweets_df_psa, file_path)
+else:
+    tweets_df_psa = pd.read_csv(file_path)
+    tweets_df_psa.head()
 
-# Read saved tweets dataset
-#tweets_df_psa = pd.read_csv("fetched_tweets/tweets_psa_{??}.csv")
-#tweets_df_psa.head()
-
-# Process
-# process_df("psa").head()
-# TODO tweets_df_psa["Stance_weighted_reach"].describe().T
+process_df("psa", tweets_df_psa).head()
+tweets_df_psa["Stance_weighted_0to100"].describe().T
 
 # Manual tweaks specific to this dataset
 """
 #df.loc[df["Tweet"].str.contains("firstever global ai summit last year")]
 change_classification(tweets_df_psa, "cess_storage excited join", "Capabilities")
 change_classification(tweets_df_psa, "firstever global ai summit last year", "Safety")
-change_classification(tweets_df_psa, "let touch base rgt rgt governance token revoxs", "Capabilities")
-change_classification(tweets_df_psa, "claude 35 sonnet transformed research paper", "Capabilities")
-change_classification(tweets_df_psa, "ai face growing censorship", "Safety")
-change_classification(tweets_df_psa, "zayaai brings future healthcare within", "Capabilities")
-change_classification(tweets_df_psa, "web3 adoption skyrocketing aicrypto project boasting", "Capabilities")
-change_classification(tweets_df_psa, "initial reaction overstated poking around morning", "Capabilities")
-change_classification(tweets_df_psa, "ai theft stealing voice", "Capabilities")
-change_classification(tweets_df_psa, "masa x gt protocol exciting news masa", "Capabilities")
-change_classification(tweets_df_psa, "excited kick broadband commission working group data governance digital age data", "Safety")
-change_classification(tweets_df_psa, "policymakers laid thoughtful framework regulation promote responsible", "Safety")
-change_classification(tweets_df_psa, "malicious abuse ai example no2 vulnerability", "Capabilities")
-change_classification(tweets_df_psa, "generative ai continues misused abused malicious individual", "Capabilities")
-change_classification(tweets_df_psa, "first came apple product classified", "Capabilities")
-
 recalculate_reclassification(tweets_df_psa)
 """
-
 # tweets_df_psa.groupby("Classification")["Stance_0to100"].agg(["count", "mean"])
 
 # Final score of PSA
 hist_psa = [62.34, 63.56, 65.12, 66.78, 67.89, 68.45, 68.90, 69.12, 69.45, 69.78, 70.12, 70.50, 70.77]
-# final_psa = tweets_df_psa["Stance_0to100"].mean() # 70.77
-final_psa = 70.77
+final_psa = tweets_df_psa["Stance_weighted_0to100"].mean().round(2)
+hist_psa.append(final_psa)
 
 
-
-## INVESTMENTS
-hist_invcap = [95.14, 95.53, 95.39, 95.35, 97.02, 97.71, 97.67, 97.03, 95.93, 96.04, 96.03, 94.78, 92.94]
-hist_invsaf = [87.97, 86.03, 82.53, 81.31, 80.42, 75.87, 80.56, 85.30, 84.21, 88.92, 88.45, 88.18, 76.88]
+# INVESTMENTS
+hist_invcap = [95.39, 95.35, 97.02, 97.71, 97.67, 97.03, 95.93, 96.04, 96.03, 94.96, 94.84, 94.01, 95.29, 94.44]
+hist_invsaf = [17.47, 18.69, 19.58, 24.13, 19.44, 14.70, 15.79, 11.08, 11.55, 11.57, 21.16, 30.41, 30.76, 26.54]
 
 final_invcap = hist_invcap[-1]
 final_invsaf = hist_invsaf[-1]
 
 
-## FINAL TOTAL CALCULATION
-hist_airi = [0.25 * (hist_rsa[i] + hist_psa[i] + hist_invcap[i] + hist_invsaf[i]) for i in range(len(hist_rsa))]
+# FINAL TOTAL CALCULATION
+hist_airi = [round(0.25 * (hist_rsa[i] + hist_psa[i] + hist_invcap[i] + hist_invsaf[i]), 2) for i in range(len(hist_rsa))]
 airi_score = round(0.25*(final_psa + final_rsa + final_invcap + final_invsaf), ndigits=2)
-
-
-"""
-### Little tests to check if it works
-df["Tweet"].head()
-
-sia = SentimentIntensityAnalyzer()
-sia.polarity_scores("We're going to get more powerful AI systems, and they will inevitably diffuse more over time. Rather than just focusing on limiting capabilities, we should focus on adapting to their impacts.")
-sia.polarity_scores("AI safety is very important, it deserves great investment and effort and I am excited to work in this field.")
-sia.polarity_scores("AI capabilities is very important, it deserves great investment and effort and I am excited to work in this field.")
-sia.polarity_scores("While AI safety is important, putting up regulations to ensure it limits human mind and maybe we should see what our smartest can do with AI.")
-
-df2 = pd.DataFrame({
-    "Tweet": [
-        "Even if use finetuning, we cannot guarantee AI safety.",
-        "Finetuning should work to ensure we can continue working on AI and make it more capable.",
-        "'alignment', 'robustness', 'bias', 'fair', 'fairness', 'interpretability', 'explainability', 'transparency', 'transparent', 'accountability', 'oversight', 'trustworthiness', 'trustworthy', 'adversarial', 'mitigation', 'compliance', 'compliant', 'standard', 'audit', 'governance', 'best practice', 'ethical AI', 'AI principle', 'human-in-the-loop'",
-        "'model drift', 'dataset bias', 'overfitting', 'underfitting', 'training data', 'validation data', 'fine-tuning', 'pre-training', 'prompt engineering', 'reinforcement learning', 'reward modeling', 'human feedback', 'differential privacy', 'federated learning', 'adversarial attacks', 'defense mechanism', 'robustness check', 'anomaly detection', 'safety protocol', 'kill switch', 'fail-safe', 'control system', 'constraint satisfaction', 'value alignment', 'scalable oversight', 'corrigibility', 'interpretability tool', 'post-hoc analysis', 'causality', 'counterfactual reason', 'predictive accuracy', 'performance metric'",
-        "'AI policy', 'legislative framework', 'compliance standard', 'AI ethic board', 'regulatory compliance', 'risk assessment', 'safety standard', 'policy recommendation', 'AI oversight', 'international cooperation', 'AI treaty'",
-        "Artificial intelligence safety is very important, it deserves great investment and effort and I am excited to work in this field.",
-        "AI capabilities is very important, it deserves great investment and effort and I am excited to work in this field.",
-        "While AI safety is important, putting up regulations to ensure it limits human mind and maybe we should see what our smartest can do with AI.",
-        "It's good that technological innovations are being limited by AI policies.",
-        "engineering learning robustness regulatory compliance Legislative frameworks, protocols counterfactual treaties compliance standards, AI ethics boards. Many AI Metrics are here."]})
-
-def process_df2(dataframe, safety_keywords, capability_keywords):
-    # Normalizing Case Folding
-    dataframe['Tweet'] = dataframe['Tweet'].str.lower()
-
-    # Punctuations
-    dataframe['Tweet'] = dataframe['Tweet'].str.replace('[^\w\s]', '', regex=True)
-
-    # Numbers
-    dataframe['Tweet'] = dataframe['Tweet'].str.replace('\d', '')
-
-    # Stopwords
-    # nltk.download('stopwords')
-    sw = stopwords.words('english')
-    dataframe['Tweet'] = dataframe['Tweet'].apply(lambda x: " ".join(x for x in str(x).split() if x not in sw))
-
-    # Rarewords
-    # TODO cancelled Rareword processing because it's a small dataset and I don't want to lose words
-    # temp_df = pd.Series(' '.join(dataframe['Tweet']).split()).value_counts()
-    # drops = temp_df[temp_df <= 1]
-    # dataframe['Tweet'] = dataframe['Tweet'].apply(lambda x: " ".join(x for x in x.split() if x not in drops))
-
-    # Lemmatization
-    # nltk.download('wordnet')
-    dataframe['Tweet'] = dataframe['Tweet'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
-
-    # Sentiment Analysis
-    sia = SentimentIntensityAnalyzer()
-    dataframe["polarity_score"] = dataframe["Tweet"].apply(lambda x: sia.polarity_scores(x)["compound"])
-
-    dataframe["Classification"] = dataframe["Tweet"].apply(classify_tweet, args=(safety_keywords, capability_keywords))
-
-    # Stance_-1to1 is the compound score of SIA, adjusted to be POS for pro-AI-Capabilities and NEG for pro-AI-Safety
-    dataframe["Stance_-1to1"] = dataframe.apply(
-        lambda row: -row["polarity_score"] if row["polarity_score"] > 0 and row["Classification"] == "Safety" else row[
-            "polarity_score"] if row["polarity_score"] < 0 and row["Classification"] == "Capabilities" else row[
-            "polarity_score"], axis=1)
-
-    # Normalize Stance_-1to1 to between 0 to 100
-    dataframe["Stance_0to100"] = ((dataframe["Stance_-1to1"] + 1) / 2 * 100).round(2)
-    # TODO add line to accommodate retweet count bc I will pull once more without RTs but with just the RT numbers
-    return dataframe
-
-process_df2(df2, safety_keywords_psa, capability_keywords_psa).head()
-
-
-########### VISUALISATION
-
-# Calculating term frequencies
-tf = df["Tweet"].apply(lambda x: pd.value_counts(x.split(" "))).sum(axis=0).reset_index()
-tf.columns = ["words", "tf"]
-tf.sort_values("tf", ascending=False)
-
-# Barplot
-tf[tf["tf"] > 200].plot.bar(x="words", y="tf")
-plt.show()
-
-# Plot the distribution of stances
-sns.countplot(x='Stance', data=tweets_df)
-plt.title('Stance Analysis of AI Governance Tweets')
-plt.xlabel('Stance')
-plt.ylabel('Number of Tweets')
-plt.show()
-
-# Plot the distribution of sentiments
-sns.countplot(x='Sentiment_Class', data=tweets_df)
-plt.title('Sentiment Analysis of AI Governance Tweets')
-plt.xlabel('Sentiment')
-plt.ylabel('Number of Tweets')
-plt.show()
-
-# Key concerns (most common words in all)
-all_tweets = ' '.join(tweets_df['Tweet'])
-wordcloud = WordCloud(width=800, height=400, max_words=100).generate(all_tweets)
-
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title('Word Cloud of AI Governance Tweets')
-plt.show()
-
-# Key concerns (most common words) for each stance
-for stance in ['AI Capabilities', 'AI Safety']:
-    stance_tweets = ' '.join(tweets_df[tweets_df['Stance'] == stance]['Tweet'])
-    wordcloud = WordCloud(width=800, height=400, max_words=100).generate(stance_tweets)
-
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    plt.title(f'Word Cloud of {stance} Tweets')
-    plt.show()
-"""
